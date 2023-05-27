dfb5ccac284c90df246963f38e1b9559bf9fa6f7
Pheonix0101 <purushottam5252@gmail.com>
2023-05-27 20:28:42 +0530

    Conntected to MongoDB atlas

--- controllers/admin.js
+++ controllers/admin.js
@@ -13,13 +13,9 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  req.user
-    .createProduct({
-      title: title,
-      price: price,
-      imageUrl: imageUrl,
-      description: description,
-    })
+  const product = new Product(title, imageUrl, price, description);
+  product
+    .save()
     .then((response) => {
       // console.log(res);
       console.log(`product created`);
@@ -28,75 +24,75 @@ exports.postAddProduct = (req, res, next) => {
     .catch((err) => console.log(err));
 };
 
-exports.getEditProduct = (req, res, next) => {
-  const editMode = req.query.edit;
-  if (!editMode) {
-    return res.redirect("/");
-  }
-  const prodId = req.params.productId;
-  req.user
-    .getProducts({ where: { id: prodId } })
-    .then((products) => {
-      const product = products[0];
-      // })
-      // Product.findByPk(prodId)
-      //   .then((product) => {
-      if (!product) {
-        return res.redirect("/");
-      }
-      res.render("admin/edit-product", {
-        pageTitle: "Edit Product",
-        path: "/admin/edit-product",
-        editing: editMode,
-        product: product,
-      });
-    })
-    .catch((err) => console.log(err));
-};
+// exports.getEditProduct = (req, res, next) => {
+//   const editMode = req.query.edit;
+//   if (!editMode) {
+//     return res.redirect("/");
+//   }
+//   const prodId = req.params.productId;
+//   req.user
+//     .getProducts({ where: { id: prodId } })
+//     .then((products) => {
+//       const product = products[0];
+//       // })
+//       // Product.findByPk(prodId)
+//       //   .then((product) => {
+//       if (!product) {
+//         return res.redirect("/");
+//       }
+//       res.render("admin/edit-product", {
+//         pageTitle: "Edit Product",
+//         path: "/admin/edit-product",
+//         editing: editMode,
+//         product: product,
+//       });
+//     })
+//     .catch((err) => console.log(err));
+// };
 
-exports.postEditProduct = (req, res, next) => {
-  const prodId = req.body.productId;
-  const updatedTitle = req.body.title;
-  const updatedPrice = req.body.price;
-  const updatedImageUrl = req.body.imageUrl;
-  const updatedDesc = req.body.description;
+// exports.postEditProduct = (req, res, next) => {
+//   const prodId = req.body.productId;
+//   const updatedTitle = req.body.title;
+//   const updatedPrice = req.body.price;
+//   const updatedImageUrl = req.body.imageUrl;
+//   const updatedDesc = req.body.description;
 
-  Product.findByPk(prodId)
-    .then((product) => {
-      product.title = updatedTitle;
-      product.price = updatedPrice;
-      product.description = updatedDesc;
-      product.imageUrl = updatedImageUrl;
-      return product.save();
-    })
-    .then((response) => {
-      res.redirect("/admin/products");
-      console.log(`product updated: ${response}`);
-    });
-};
+//   Product.findByPk(prodId)
+//     .then((product) => {
+//       product.title = updatedTitle;
+//       product.price = updatedPrice;
+//       product.description = updatedDesc;
+//       product.imageUrl = updatedImageUrl;
+//       return product.save();
+//     })
+//     .then((response) => {
+//       res.redirect("/admin/products");
+//       console.log(`product updated: ${response}`);
+//     });
+// };
 
-exports.getProducts = (req, res, next) => {
-  // Product.findAll()
-  req.user.getProducts()
-    .then((products) => {
-      res.render("admin/products", {
-        prods: products,
-        pageTitle: "Admin Products",
-        path: "/admin/products",
-      });
-    })
-    .catch((err) => console.log(err));
-};
+// exports.getProducts = (req, res, next) => {
+//   // Product.findAll()
+//   req.user.getProducts()
+//     .then((products) => {
+//       res.render("admin/products", {
+//         prods: products,
+//         pageTitle: "Admin Products",
+//         path: "/admin/products",
+//       });
+//     })
+//     .catch((err) => console.log(err));
+// };
 
-exports.postDeleteProduct = (req, res, next) => {
-  const prodId = req.body.productId;
-  Product.findByPk(prodId)
-    .then((product) => {
-      return product.destroy();
-    })
-    .then((result) => {
-      console.log("product deleted");
-      res.redirect("/admin/products");
-    })
-    .catch((err) => console.error());
-};
+// exports.postDeleteProduct = (req, res, next) => {
+//   const prodId = req.body.productId;
+//   Product.findByPk(prodId)
+//     .then((product) => {
+//       return product.destroy();
+//     })
+//     .then((result) => {
+//       console.log("product deleted");
+//       res.redirect("/admin/products");
+//     })
+//     .catch((err) => console.error());
+// };
--- util/database.js
+++ util/database.js
@@ -1,8 +1,26 @@
-const Sequelize = require("sequelize");
+const mongodb = require("mongodb");
 
-const sequelize = new Sequelize("node-complete", "root", "nodecomplete", {
-  dialect: "mysql",
-  host: "localhost",
-});
+const MongoClient = mongodb.MongoClient;
+let _db;
 
-module.exports = sequelize;
+const mongoConnect = (callBack) => {
+  MongoClient.connect(
+    "mongodb+srv://purushottam:Oc07QUuUv4vMFMOf@cluster0.rzbqmrj.mongodb.net/shop?retryWrites=true&w=majority"
+  )
+    .then((client) => {
+      console.log("connected to database successfully");
+      _db = client.db();
+      callBack();
+    })
+    .catch((err) => console.log(err));
+};
+
+const getDb = () => {
+  if (_db) {
+    return _db;
+  }
+  throw "No database found";
+};
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
